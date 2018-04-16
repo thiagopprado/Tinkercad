9b59c17f145fb128bbb4139930fbef37357e1c5f
Thiago Pereira do Prado <thiagopereiraprado@gmail.com>
2018-04-16 12:53:11 -0300

    Update LED RGB.ino

--- LED RGB.ino
+++ LED RGB.ino
@@ -11,8 +11,8 @@ void setup(){
 }
 
 void loop(){
-  	analogWrite(R, random(255)); //Ligando as portas PWM com valor aleatório de duty cicle
-    analogWrite(G, random(255));
+	analogWrite(R, random(255)); //Ligando as portas PWM com valor aleatório de duty cicle
+	analogWrite(G, random(255));
 	analogWrite(B, random(255));
-    delay(200);	//Espera 200ms até atualizar o LED com novos valores aleatórios
+	delay(200); //Espera 200ms até atualizar o LED com novos valores aleatórios
 }
